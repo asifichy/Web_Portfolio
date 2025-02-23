44b725a7febbf4f70e03e5670132a4a0e40e4a21
asifichy <asifimtiyajchowdhury@gmail.com>
2025-02-23 23:01:10 +0600

    adding more components for header section

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { path: '/', name: 'home' },
+  { path: '/projects', name: 'my projects' },
+  { path: '/contact', name: 'contact' },
+];
+
 const Nav = ({containerStyles, linkStyles, underlineStyles}) => {
+  const pathname = usePathname();
   return (
     <nav className={`${containerStyles}`}>
-        Nav
+        {links.map((link, index) => {
+          return (
+            <Link
+              href={link.path}
+              key={index}
+              className={`capitalize ${linkStyles}`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
     </nav>
   )
 }
 
-export default Nav
+export default Nav;
