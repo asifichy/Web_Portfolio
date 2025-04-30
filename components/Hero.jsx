e1c6afaa053e9b3c93667ccf02930ffd55cb4198
asifichy <asifimtiyajchowdhury@gmail.com>
2025-04-30 16:22:21 +0600

    modification

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Download, Send } from "lucide-react";
+
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine
+} from "react-icons/ri";
+
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="py-12 xl:py-24 h-[84vh] xh:pt-28 bg-hero bg-no-repeat ng-bottom dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          {/* text */}
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[5px]">
+              Cloud & DevOps Engineer
+            </div>
+            <h1 className="h1 mb-4">
+              Hello, my name is Md. Asif Imtiyaj Chowdhury
+            </h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Brief description with insights of myself
+            </p>
+            {/* button */}
+            <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contact Me <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                  Download CV <Download size={18} />
+                </Button> 
+            </div>
+          </div>
+          {/* image */}
+          <div className="hidden xl:flex relative">image</div>
+        </div>
+        {/* icon */}
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
