05638f5f05ed003714f6ba410aa43cd691f42de9
asifichy <asifimtiyajchowdhury@gmail.com>
2025-04-30 12:18:11 +0600

    bug fix in themeprovider in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,10 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute='class' defaultTheme='light'/>
+        <ThemeProvider attribute='class' defaultTheme='light'>
           <Header />
           {children}
           <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
