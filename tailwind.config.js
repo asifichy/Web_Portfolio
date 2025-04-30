14e829362b39d1b82b9bac04d292f0f1876c91b4
asifichy <asifimtiyajchowdhury@gmail.com>
2025-04-30 16:24:20 +0600

    style modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "class",
+    darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container: {
+		center: true,
+		padding: "2rem",
+		screens: {
+			"2xl": "1400px",
+		},
+	},
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
@@ -54,8 +61,25 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+  		},
+		keyframes: {
+			"accordion-down": {
+				from: { height: 0 },
+				to: { height: "var(--radix-accordion-content-height)" },
+			},
+			"accordion-up": {
+				from: { height: "var(--radix-accordion-content-height)" },
+				to: { height: 0 },
+			},
+		},
+		animation: {
+			"accordion-down": "accordion-down 0.2s ease-out",
+			"accordion-up": "accordion-up 0.2s ease-out",
+		},
+		backgroundImage: {
+			hero: 'url(/hero/hero-bg.png)'
+		}
+  	},
   },
   plugins: [require("tailwindcss-animate")],
 };
