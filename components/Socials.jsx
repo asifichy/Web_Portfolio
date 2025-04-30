893f00ebd58f3d10ca6c7873b01c59ab9aec6b93
asifichy <asifimtiyajchowdhury@gmail.com>
2025-04-30 18:50:05 +0600

    adding social links demo

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
 
-const Socials = () => {
+import {
+  RiLinkedinFill,
+  RiGithubFill,
+  RiFacebookFill,
+  RiGitlabFill,
+  RiTwitterFill,
+  RiMediumFill,
+  RiNotionFill,
+} from 'react-icons/ri';
+
+import Link from "next/link";
+
+const icons = [
+  {
+    path: "/",
+    name: <RiLinkedinFill />
+  },
+  {
+    path: "/",
+    name: <RiGithubFill />
+  },
+  {
+    path: "/",
+    name: <RiGitlabFill />
+  },
+  {
+    path: "/",
+    name: <RiFacebookFill />
+  },
+  {
+    path: "/",
+    name: <RiTwitterFill />
+  },
+  {
+    path: "/",
+    name: <RiMediumFill />
+  },
+  {
+    path: "/",
+    name: <RiNotionFill />
+  },
+];
+
+const Socials = ({ containerStyles, iconsStyles }) => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link
+            href={icon.path}
+            key={index}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Socials;
