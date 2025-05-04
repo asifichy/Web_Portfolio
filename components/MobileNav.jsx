47eb610090ef73c724aafeb2b3a344d13e6897f8
asifichy <asifimtiyajchowdhury@gmail.com>
2025-05-05 01:34:45 +0600

    mobile nav with links and css

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,31 @@
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+
+import Nav from "./Nav";
+import Logo from "./Logo";
+import Socials from "./Socials";
+
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+          </div>
+          <Socials containerStyles="flex gap-x-4" iconsStyle="text-2xl" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
