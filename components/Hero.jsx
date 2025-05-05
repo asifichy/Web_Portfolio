6b516b5034aeca453c3049f115c88149c5953bd1
asifichy <asifimtiyajchowdhury@gmail.com>
2025-05-05 17:25:37 +0600

    bug fix with css var in jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,7 +15,7 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section className="py-12 xl:py-24 h-[90vh] xh:pt-28 bg-hero bg-no-repeat bg-bottom dark:bg-none">
+    <section className="py-12 xl:py-24 h-[90vh] xl:pt-20 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* text */}
