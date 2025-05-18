73b9b11ae4e836105b76f3118389564887092323
asifichy <asifimtiyajchowdhury@gmail.com>
2025-05-18 16:47:50 +0600

    review component added

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+import Reviews from "@/components/Reviews";
 
 export default function Home() {
   return (
