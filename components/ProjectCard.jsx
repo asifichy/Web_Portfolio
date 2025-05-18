c6c0c84e0199f5ba38dae08a89de90378cbed247
asifichy <asifimtiyajchowdhury@gmail.com>
2025-05-18 16:41:31 +0600

    porject info added yet more to update

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,16 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardHeader } from "./ui/card";
-import { Github, Gitlab, Link2Icon } from "lucide-react";
+import { Github, Link2Icon } from "lucide-react";
 import { Badge } from "./ui/badge";
 
 const ProjectCard = ({ project }) => {
   return (
-    <Card>
+    <Card className="group overflow-hidden relative">
       <CardHeader className="p-0">
         {/* image */}
-        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:bg-[110%]">
-            <Image className="absolute bottom-0 shadow-2xl" src={project.image} alt="" width={250} height={250} />
+        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
+          <Image
+            className="absolute bottom-0 shadow-2xl"
+            src={project.image}
+            alt=""
+            width={247}
+            height={250}
+          />
+          {/* buttons links */}
+          <div className="flex gap-x-4">
+            <Link
+              href={project.link}
+              className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+            >
+              <Link2Icon className="text-white" />
+            </Link>
+            <Link
+              href={project.github}
+              className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+            >
+              <Github className="text-white" />
+            </Link>
+          </div>
         </div>
       </CardHeader>
       <div className="h-full px-8 py-6">
