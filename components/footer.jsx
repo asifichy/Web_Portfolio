bb9e41855fe560444189838188b89d8a88d78dcf
asifichy <asifimtiyajchowdhury@gmail.com>
2025-05-18 17:55:10 +0600

    footer added

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,23 @@
+import Socials from "@/components/Socials";
+
 const Footer = () => {
   return (
-    <footer>footer</footer>
-  )
-}
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-primary dark:text-white/70 text-[25px] hover:text-white dark:hover:text-primary transition-all"
+          />
+          {/* copywrite */}
+          <div className="text-muted-foreground text-[20px]">
+            Copyright &copy; Md. Asif Imtiyaj Chowdhury. All right reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
