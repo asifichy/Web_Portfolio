c3338771feff398bf0f2e3781c44f57794540af7
asifichy <asifimtiyajchowdhury@gmail.com>
2026-05-18 00:34:06 +0600

    feat: enhance Template component with improved animations and transition effects

--- app/template.jsx
+++ app/template.jsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 import userScrollProgress from '@/hooks/userScrollProgress';
 
 const variants = {
-    hidden: { opacity: 0 },
-    enter: { opacity: 1 },
+    hidden: { opacity: 0, y: 20 },
+    enter: { opacity: 1, y: 0 },
 };
 
 const Template = ({ children }) => {
@@ -16,17 +16,16 @@ const Template = ({ children }) => {
         variants={variants}
         initial='hidden'
         animate='enter'
-        transition={{ type: 'linear', delay: 0.2, duration: 0.4 }}
+        transition={{ type: 'tween', delay: 0.1, duration: 0.5 }}
       >
         {children}
       </motion.main>
       {/* Completion bar */}
-      <span
+      <motion.span
         style={{ transform: `translateY(${completion - 100}%)` }}
-        className='fixed z-50 bg-primary w-1 top-0 right-0
-        bottom-0 transition-all duration-700'
-      ></span>
-      {/* <div className='h-[4000px]'></div> */}
+        className='fixed z-50 bg-gradient-to-b from-primary to-orange-500 w-1 top-0 right-0
+        bottom-0 transition-all duration-700 rounded-full'
+      ></motion.span>
     </>
   );
 }
